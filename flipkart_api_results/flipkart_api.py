4e44f7862bcc1e3179fb764e8f98e4024d89a413
AZHARULLAH SHARIFF <azhar@AZHARULLAHs-Air.connectify>
2016-03-14 04:35:59 +0530

    Flipkart API working

---
+++ flipkart_api_results/flipkart_api.py
@@ -0,0 +1,37 @@
+import subprocess
+import json
+from pprint import pprint
+
+f = open("output", "w")
+process = subprocess.call(['curl',  '-H', 'Fk-Affiliate-Id:shariffaz', '-H', 'Fk-Affiliate-Token:c569d5da22704c278e90af8226c42174', 'https://affiliate-api.flipkart.net/affiliate/search/json?query=iphone&resultCount=20'], stdout=f)
+
+with open('output') as data_file:
+	data = json.load(data_file)
+
+# pprint(data)
+# print "\n\n*****************\n\n"
+
+# i = 0
+# j = 0
+# allprods = [[]]
+for value in data["productInfoList"]:
+	prodcat = value['productBaseInfo']['productIdentifier']['categoryPaths']['categoryPath'][0][0]['title']
+	prodid = value['productBaseInfo']['productIdentifier']['productId']
+	prodtitle = value['productBaseInfo']['productAttributes']['title']
+	prodimgurl = value['productBaseInfo']['productAttributes']['imageUrls']['unknown']
+	prodmrp = value['productBaseInfo']['productAttributes']['maximumRetailPrice']['amount']
+	prodsp = value['productBaseInfo']['productAttributes']['sellingPrice']['amount']
+	produrl = value['productBaseInfo']['productAttributes']['productUrl']
+	prodbrand = value['productBaseInfo']['productAttributes']['productBrand']
+
+	print prodcat
+	print prodid
+	print prodtitle
+	print prodimgurl
+	print prodmrp
+	print prodsp
+	print produrl
+	print prodbrand
+	print "\n\n*****************\n\n"
+
+	# allprods[i] = array(prodid,prodcat,prodtitle,prodimgurl,prodmrp,prodsp,produrl,prodbrand)
